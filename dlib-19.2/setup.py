e049737e0968ccc87a0c2eb144497ae8b781581a
doitforyou <doitforyou@daum.net>
2017-03-21 21:52:08 +0900

    tt

--- dlib-19.2/setup.py
+++ dlib-19.2/setup.py
@@ -1,4 +1,4 @@
-1234"""setup for the dlib project
+가나다라setup for the dlib project
  Copyright (C) 2015  Ehsan Azar (dashesy@linux.com)
  License: Boost Software License   See LICENSE.txt for the full license.
 
